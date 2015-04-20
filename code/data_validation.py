6517e26b95db0c2fbb81555db99f975a5457ed68
tengr <tengrc1106@gmail.com>
2015-04-20 15:18:05 +1000

    Update data_validation.py

--- code/data_validation.py
+++ code/data_validation.py
@@ -1,15 +1,13 @@
 #ord('~')
 
 input = "|name|address|~n|Annie|a@b.com|sdsf.com|~n|brian|sdsdd|~n"
-input = "|name|address|~n|Patrick|patrick@test.com|pat@test.com|~n|Annie|Annie@test.com|~n"
+input = "|name|address|~n|Patrick|patrick@test.com|~~pat@test.com|~n|Annie|Annie@test.com|~n"
 
-if input[0] != '|'
-    print("0:0:0:format_error")
-    
 pos = input.find('|~n',1)
 fnms = input[1:pos].split('|')
 nmfnms = len(fnms)
 last_name = fnms[-1]
+
 # print (fnms[-1])
 
 # print (len(fnms))
@@ -37,7 +35,7 @@ while True:
             if len(field) > 0:
                 nonempty += 1
                 for i in range(len(field)):
-                    if ord(field[i]) < ord(' ') or ord(field[i]) > ord('~'):
+                    if ord(field[i]) < ord(' ') or ord(field[i]) >= ord('~'):
                         print("0:0:0:format_error")
 
 
@@ -48,3 +46,26 @@ if extra > 0:
     last_name += ('_'+str(extra))
 
 print(str(records) + ":"+str(nmfds) + ":" + str(empty) + ":" + last_name)
+
+
+
+
+#print("|name|address|Annie|sdsf|~n".split("|"))
+
+# print (inst[1:inst.find("|~n")].split("|"))
+
+# start = inst.find("|~n") + 4
+# pos = inst[start:].find("|~n")
+
+# print(inst[start:pos])
+
+# print (inst[start:pos].split("|"))
+
+# print(inst[start:pos].split("|"))
+
+# line = inst[start:pos]
+
+# fields = line.split("|")
+
+# print (len(fields))
+# print (fields[0])
