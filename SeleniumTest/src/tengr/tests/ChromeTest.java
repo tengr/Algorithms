c2a2554cedd5f629dc8ded3b22c633d165cde788
Ruichen Teng <ruichent@au1.ibm.com>
2016-10-10 14:05:01 +0800

    update

--- SeleniumTest/src/tengr/tests/ChromeTest.java
+++ SeleniumTest/src/tengr/tests/ChromeTest.java
@@ -1,8 +1,10 @@
 package tengr.tests;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
  
 public class ChromeTest extends TestBase{
   	@Override
-  	public void setBrowser() {
+    public void setBrowser(DesiredCapabilities caps) {
   		caps.setCapability("platform", "CAPITAN");
   	    caps.setCapability("version", "beta");
   	    caps.setCapability("browserName", "chrome");
