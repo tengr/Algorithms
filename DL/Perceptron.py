a8c5e37d106d196c486757b2704f80a343640a12
Ruichen Teng <tengrc1106@gmail.com>
2017-04-29 16:52:18 +1000

    stochastic gradient descent

--- DL/Perceptron.py
+++ DL/Perceptron.py
@@ -28,17 +28,34 @@ learning_rate = 0.1
 # 		delta_b = learning_rate * (data[2] - y)
 # 		b += delta_b
 
-#gradient descent
-for i in xrange(100000):
-	w0_delta = learning_rate * sum((step(cross_prod(w, data[:2]) + b) - data[2]) * data[0] for data in train_data)
-	w1_delta = learning_rate  * sum((step(cross_prod(w, data[:2]) + b) - data[2]) * data[1] for data in train_data)
-	b_delta = learning_rate  * sum((step(cross_prod(w, data[:2]) + b) - data[2]) * 1 for data in train_data)
-	w[0] -= w0_delta
-	w[1] -= w1_delta
-	b -= b_delta
+def gradient_descent():
+	global b
+	global w
+	for i in xrange(1000):
+		w0_delta = learning_rate * sum((step(cross_prod(w, data[:2]) + b) - data[2]) * data[0] for data in train_data)
+		w1_delta = learning_rate  * sum((step(cross_prod(w, data[:2]) + b) - data[2]) * data[1] for data in train_data)
+		b_delta = learning_rate  * sum((step(cross_prod(w, data[:2]) + b) - data[2]) * 1 for data in train_data)
+		w[0] -= w0_delta
+		w[1] -= w1_delta
+		b -= b_delta
+
+def stochastic_gradient_descent():
+	global b
+	global w
+	for i in xrange(1000):
+		for data in train_data:
+			w0_delta = learning_rate * (step(cross_prod(w, data[:2]) + b) - data[2]) * data[0]
+			w1_delta = learning_rate  * (step(cross_prod(w, data[:2]) + b) - data[2]) * data[1]
+			b_delta = learning_rate  *  (step(cross_prod(w, data[:2]) + b) - data[2]) * 1
+			w[0] -= w0_delta
+			w[1] -= w1_delta
+			b -= b_delta
 
 def predict(x):
 	return step(cross_prod(w,x) + b)
+
+
+stochastic_gradient_descent()
 
 print w
 print b	
